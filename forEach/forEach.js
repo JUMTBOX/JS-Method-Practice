916c6f8a438b5a85d26eff3f523ef9b7780247bc
JUMTBOX <yjey12@naver.com>
2023-01-29 21:56:58 +0900

    자바스크립트 메소드 연습

---
+++ forEach/forEach.js
@@ -0,0 +1,34 @@
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// numbers.forEach((el) => {
+//   if (el % 2 === 0) {
+//     console.log(el);
+//   }
+// });
+
+// for (let el of numbers) {
+//   console.log(el);
+// }
+
+const movies = [
+  {
+    title: "amadeus",
+    score: 99,
+  },
+  {
+    title: "stand by me",
+    score: 85,
+  },
+  {
+    title: "parasite",
+    score: 95,
+  },
+  {
+    title: "alien",
+    score: 90,
+  },
+];
+
+movies.forEach((movie) => {
+  console.log(`${movie.title}의 점수는 ${movie.score}점 입니다.`);
+});
